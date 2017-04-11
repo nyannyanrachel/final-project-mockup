c2b88ef2d63ea338c26577f2a9d0efec7a8fccda
evanyasaka <eyasaka@hawaii.edu>
2017-04-11 09:22:50 -1000

    Removed superfluous pages

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -1,31 +1,10 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
-/*FlowRouter.route('/', {
-  name: 'Home_Page',
-  action() {
-    BlazeLayout.render('App_Body', { main: 'Home_Page' });
-  },
-});*/
-
 FlowRouter.route('/', {
   name: 'Landing_Page',
   action() {
     BlazeLayout.render('App_Body', { main: 'Landing_Page' });
-  },
-});
-
-FlowRouter.route('/list', {
-  name: 'List_Stuff_Page',
-  action() {
-    BlazeLayout.render('App_Body', { main: 'List_Stuff_Page' });
-  },
-});
-
-FlowRouter.route('/add', {
-  name: 'Add_Stuff_Page',
-  action() {
-    BlazeLayout.render('App_Body', { main: 'Add_Stuff_Page' });
   },
 });
 
@@ -57,12 +36,6 @@ FlowRouter.route('/student-home-page', {
   },
 });
 
-FlowRouter.route('/stuff/:_id', {
-  name: 'Edit_Stuff_Page',
-  action() {
-    BlazeLayout.render('App_Body', { main: 'Edit_Stuff_Page' });
-  },
-});
 
 FlowRouter.notFound = {
   action() {
--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -1,22 +1,13 @@
-import './add-stuff-page.html';
-import './add-stuff-page.js';
+/*------------------------------------------------------------------
+When adding imports order alphabetically to make finding them in the
+file tree easier.
+------------------------------------------------------------------*/
 
 import './approve-quest-page.html';
 import './approve-quest-page.js';
 
-import './add-quest-page.html';
-import './add-quest-page.js';
-
 import './create-a-quest-page.html';
 import './create-a-quest-page.js';
-
-import './edit-stuff-page.html';
-import './edit-stuff-page.js';
-
-import './home-page.html';
-
-import './list-stuff-page.html';
-import './list-stuff-page.js';
 
 import './landing-page.html';
 import './landing-page.js';
